01ff74f4c0d806a27ce5f5082b164f456793cb16
karolaynemattei-cmyk <karolayne.mattei@escola.pr.gov.br>
2026-08-18 11:02:11 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const likeButtons = document.querySelectorAll('.btn-like');
 
+    likeButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            const countSpan = button.querySelector('.count');
+            let currentLikes = parseInt(countSpan.textContent, 10);
+
+            if (button.classList.contains('liked')) {
+                button.classList.remove('liked');
+                countSpan.textContent = currentLikes - 1;
+            } else {
+                button.classList.add('liked');
+                countSpan.textContent = currentLikes + 1;
+            }
+        });
+    });
+});
